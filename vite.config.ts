123d04fe17d17e083a9d6e495dd05fb721fb38e9
Vierdant <vierdant@gmail.com>
2025-11-09 23:24:25 +0100

    hears

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 export default defineConfig({
   plugins: [svelte(), tailwindcss()],
   server: {
-    cors: {
-      origin: "https://dndsheet.onrender.com",
-      credentials: true,
-      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-      allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Accept", "Origin", "Access-Control-Allow-Origin"],
-    },
+    cors: true,
   },
 })
