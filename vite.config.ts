3d9adf0af73f955482fb71ad8580db8046d73e87
Vierdant <vierdant@gmail.com>
2025-11-09 23:14:56 +0100

    cors

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 export default defineConfig({
   plugins: [svelte(), tailwindcss()],
   server: {
-    cors: {
-      origin: "https://www.owlbear.rodeo",
-    },
+    cors: true,
   },
 })
